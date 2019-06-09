dff44f442b69ab10fa73efd1b47db287de6c0b5a
sunnysmiles17 <jadearan.iwnl@gmail.com>
2019-06-09 19:42:16 -0400

    player++

---
+++ Scripts/Player/States/PlayerHitstun.gd
@@ -0,0 +1,17 @@
+extends State
+class_name PlayerHitstun
+
+var player : Entity
+
+func initialize(context) -> void:
+	player = context as Entity
+	
+func begin() -> void:
+	player.anim_state = "idle"
+
+func update(delta : float) -> void:
+	if !player.in_hitstun():
+		if player.in_knockback():
+			_change_state("PlayerKnockback")
+		else:
+			_change_state("PlayerIdle")
