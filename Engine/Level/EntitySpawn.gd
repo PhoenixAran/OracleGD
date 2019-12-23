1b6fd5356f3151c35d631df6cc140b36a535d892
sunnysmiles17 <jadearan.iwnl@gmail.com>
2019-12-22 23:45:44 -0500

    made entity spawn more efficient by implementing custom drawing of given packed scene

--- Engine/Level/EntitySpawn.gd
+++ Engine/Level/EntitySpawn.gd
@@ -1,25 +1,39 @@
+tool
 extends Node2D
 class_name EntitySpawn
 
-var packed_entity : PackedScene
+export(PackedScene) var packed_entity setget set_packed_entity
+export(Rect2) var editor_src_rect := Rect2(0, 0, 16, 16) setget set_editor_src_rect
+#tool variables, won't be used in game
 
 func _ready() -> void:
-	var child = get_children()[0]
-	var scene_path : String = child.get("filename")
-	child.queue_free()
-	packed_entity = load(scene_path)
+	if Engine.is_editor_hint():
+		pass
 
-#Game Code
+func _draw() -> void:
+	if Engine.is_editor_hint() and packed_entity:
+		var temp_entity = packed_entity.instance()
+		var entity_sprite : EntitySprite = temp_entity.get_node_or_null("EntitySprite")
+		if entity_sprite:
+			var temp_texture := entity_sprite.texture
+			var destination_rect := Rect2(0 - (editor_src_rect.size.x / 2), 0 - (editor_src_rect.size.y / 2), editor_src_rect.size.x, editor_src_rect.size.y)
+			draw_texture_rect_region(temp_texture, destination_rect, editor_src_rect)
+		else:
+			print("Cannot find node EntitySprite for node")
+		temp_entity.queue_free()
+
+func set_packed_entity(value) -> void:
+	packed_entity = value
+	if Engine.is_editor_hint():
+		#request redraw
+		update()
+
+func set_editor_src_rect(new_rect) -> void:
+	editor_src_rect = new_rect
+	if Engine.is_editor_hint():
+		update()
+		
 func spawn() -> Entity:
-	var entity = packed_entity.instance() as Entity
+	var entity : Entity = packed_entity.instance()
 	entity.transform = transform
 	return entity
-	#entity.connect("entity_destroyed", self, "_on_entity_destroyed")
-
-
-
-#signal callbacks
-func _on_entity_destroyed(entity : Entity):
-	#TODO set item drop
-	pass
-
